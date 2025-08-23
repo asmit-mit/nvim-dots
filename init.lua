238e27b198589f9065f34f62fe5a15a80c5f672c
Asmit Paul <hiasmitpaul@gmail.com>
2025-08-23 18:57:51 +0530

    added vim_runtime in lua_ls config

--- init.lua
+++ init.lua
@@ -462,3 +462,13 @@ vim.lsp.config('pylsp', {
     }
   }
 })
+
+vim.lsp.config('lua_ls', {
+  settings = {
+    Lua = {
+      workspace = {
+        library = vim.api.nvim_get_runtime_file("", true),
+      }
+    }
+  }
+})
