a24b0015d297cd40c4aa6ea26c0ce4b12ee6a9bd
Asmit Paul <hiasmitpaul@gmail.com>
2026-05-02 03:36:28 +0530

    update dap config to use nvim-dap-view and nvim-dap-virtual-text

--- lua/plugins/nvim-dap.lua
+++ lua/plugins/nvim-dap.lua
@@ -1,21 +1,60 @@
 vim.pack.add({
   "https://github.com/mfussenegger/nvim-dap.git",
-  "https://github.com/rcarriga/nvim-dap-ui",
-  "https://github.com/nvim-neotest/nvim-nio",
+  "https://github.com/igorlfs/nvim-dap-view.git",
+  "https://github.com/theHamsta/nvim-dap-virtual-text.git",
   "https://github.com/jay-babu/mason-nvim-dap.nvim",
 })
 
-local mason_dap = require("mason-nvim-dap")
 local dap = require("dap")
-local dapui = require("dapui")
 
-mason_dap.setup({
+require("mason-nvim-dap").setup({
   automatic_installation = true,
   handlers = {
     function(config)
       require("mason-nvim-dap").default_setup(config)
     end,
   },
+})
+
+require("dap-view").setup({
+  winbar = {
+    base_sections = {
+      breakpoints = { label = "Breakpoints", keymap = "B" },
+      scopes = { label = "Scopes", keymap = "S" },
+      exceptions = { label = "Exceptions", keymap = "E" },
+      watches = { label = "Watches", keymap = "W" },
+      threads = { label = "Stack Trace", keymap = "T" },
+      repl = { label = "REPL", keymap = "R" },
+    },
+    controls = {
+      enabled = true,
+      position = "right",
+      buttons = {
+        "play",
+        "step_into",
+        "step_over",
+        "step_out",
+        "step_back",
+        "run_last",
+        "terminate",
+        "disconnect",
+      },
+    },
+  },
+  windows = {
+    size = 0.3,
+    position = "below",
+    terminal = {
+      size = 0.25,
+      position = "left",
+      hide = {},
+    },
+  },
+  switchbuf = "useopen,split",
+})
+
+require("nvim-dap-virtual-text").setup({
+  virt_text_pos = 'eol',
 })
 
 dap.configurations.cpp = {
@@ -33,44 +72,34 @@ dap.configurations.cpp = {
 
 dap.configurations.c = dap.configurations.cpp
 
-dapui.setup()
-
 vim.fn.sign_define("DapBreakpoint", { text = "" })
 
-dap.listeners.before.attach.dapui_config = function()
-  dapui.open()
-end
-dap.listeners.before.launch.dapui_config = function()
-  dapui.open()
-end
-dap.listeners.before.event_terminated.dapui_config = function()
-  dapui.close()
-end
-dap.listeners.before.event_exited.dapui_config = function()
-  dapui.close()
-end
-
 -- Debugger keymaps
-vim.keymap.set("n", "<leader>dt", dap.toggle_breakpoint, { desc = "Toggle Breakpoint" })
+vim.keymap.set("n", "<leader>dt", "<cmd>DapToggleBreakpoint<CR>", { desc = "Toggle Breakpoint" })
 
 -- Leader key mappings
-vim.keymap.set("n", "<leader>dc", dap.continue, { desc = "Continue" })
-vim.keymap.set("n", "<leader>di", dap.step_into, { desc = "Step Into" })
-vim.keymap.set("n", "<leader>do", dap.step_over, { desc = "Step Over" })
-vim.keymap.set("n", "<leader>du", dap.step_out, { desc = "Step Out" })
+vim.keymap.set("n", "<leader>dc", "<cmd>DapContinue<CR>", { desc = "Continue" })
+vim.keymap.set("n", "<leader>di", "<cmd>DapStepInto<CR>", { desc = "Step Into" })
+vim.keymap.set("n", "<leader>do", "<cmd>DapStepOver<CR>", { desc = "Step Over" })
+vim.keymap.set("n", "<leader>du", "<cmd>DapStepOut<CR>", { desc = "Step Out" })
 
 -- Function key mappings
-vim.keymap.set("n", "<F2>", dap.continue, { desc = "Continue" })
-vim.keymap.set("n", "<F3>", dap.step_into, { desc = "Step Into" })
-vim.keymap.set("n", "<F4>", dap.step_over, { desc = "Step Over" })
-vim.keymap.set("n", "<F5>", dap.step_out, { desc = "Step Out" })
+vim.keymap.set("n", "<F2>", "<cmd>DapContinue<CR>", { desc = "Continue" })
+vim.keymap.set("n", "<F3>", "<cmd>DapStepInto<CR>", { desc = "Step Into" })
+vim.keymap.set("n", "<F4>", "<cmd>DapStepOver<CR>", { desc = "Step Over" })
+vim.keymap.set("n", "<F5>", "<cmd>DapStepOut<CR>", { desc = "Step Out" })
 
-vim.keymap.set("n", "<leader>dr", dap.repl.open, { desc = "Open REPL" })
-vim.keymap.set("n", "<leader>dl", dap.run_last, { desc = "Run Last" })
-vim.keymap.set("n", "<leader>dq", function()
-  dap.terminate()
-  dapui.close()
-end, { desc = "Terminate" })
-vim.keymap.set("n", "<leader>db", dap.list_breakpoints, { desc = "List Breakpoints" })
-vim.keymap.set("n", "<leader>de", function() dap.set_exception_breakpoints({ "all" }) end,
-  { desc = "Set Exception Breakpoints" })
+-- view keymaps
+vim.keymap.set("n", "<leader>dv", "<cmd>DapViewOpen<CR>", { desc = "Open Dap View" })
+vim.keymap.set("n", "<leader>dq", "<cmd>DapViewClose<CR>", { desc = "Close Dap View" })
+vim.keymap.set("n", "<leader>dw", "<cmd>DapViewWatch<CR>", { desc = "Dap Watch" })
+vim.keymap.set("n", "<leader>dh", function()
+  require("dap.ui.widgets").hover()
+end, { desc = "Dap Hover" })
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "dap-float",
+  callback = function()
+    vim.api.nvim_buf_set_keymap(0, "n", "q", "<cmd>close!<CR>", { noremap = true, silent = true })
+  end
+})
